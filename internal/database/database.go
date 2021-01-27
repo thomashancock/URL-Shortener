a3abb634cb986a7686f6eb53ef51d785d6f8f939
thomashancock <thomas.hancock@physics.ox.ac.uk>
2021-01-27 12:02:55 +0000

    Added comments to database.go

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,15 +4,18 @@ import (
 	"errors"
 )
 
+// Database provides an interface to store and retrieve shortened URLs
 type Database interface {
 	Add(string, string) error
 	Get(string) (string, error)
 }
 
+// database implements the Database interface using a map
 type database struct {
 	db map[string]string
 }
 
+// Add adds an entry to the database (map)
 func (db *database) Add(path string, redirect string) (error) {
 	if db.db[path] != "" {
 		return errors.New("Path already in database")
@@ -21,6 +24,7 @@ func (db *database) Add(path string, redirect string) (error) {
 	return nil
 }
 
+// Get retreives an entry to the database (map)
 func (db *database) Get(path string) (string, error) {
 	redirect := db.db[path]
 	if redirect == "" {
@@ -29,6 +33,7 @@ func (db *database) Get(path string) (string, error) {
 	return redirect, nil
 }
 
+// NewDatabase creates a new database
 func NewDatabase() Database {
 	return &database{db: make(map[string]string)}
 }
