b2a0c118cab3a231e64b8823537facc26615c5ea
Thomas Hancock <thomas.hancock.2012@my.bristol.ac.uk>
2021-01-28 11:35:32 +0000

    Added more unit tests for maphandler

--- internal/handlers/maphandler_test.go
+++ internal/handlers/maphandler_test.go
@@ -32,3 +32,66 @@ func Test_MapHandler_EntryExists(t *testing.T) {
 	expected := "<a href=\"/TestEntry\">Found</a>.\n\n"
 	assert.Equal(t, rr.Body.String(), expected)
 }
+
+// Test_MapHandler_EntryExists_Log tests MapHandler's log output when the entry is in the database
+func Test_MapHandler_EntryExists_Log(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	testDB := utils.NewTestDatabase(testLog)
+
+	testDB.Add("0", "TestEntry")
+
+	mh := NewMapHandler(testLog, testDB)
+
+	req, err := http.NewRequest("GET", "/0", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	mh.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusFound)
+
+	expectedLog := "Attempting redirect on /0\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+}
+
+// Test_MapHandler_NoEntry tests MapHandler when their is no entry in the database
+func Test_MapHandler_NoEntry(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	testDB := utils.NewTestDatabase(testLog)
+
+	mh := NewMapHandler(testLog, testDB)
+
+	req, err := http.NewRequest("GET", "/0", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	mh.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+
+	expected := "No redirect registered for /0\n"
+	assert.Equal(t, rr.Body.String(), expected)
+}
+
+// Test_MapHandler_NoEntry_Log tests MapHandler's log output when their is no entry in the database
+func Test_MapHandler_NoEntry_Log(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	testDB := utils.NewTestDatabase(testLog)
+
+	mh := NewMapHandler(testLog, testDB)
+
+	req, err := http.NewRequest("GET", "/0", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	mh.ServeHTTP(rr, req)
+
+	expectedLog := "Unable to find URL for /0\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+}
