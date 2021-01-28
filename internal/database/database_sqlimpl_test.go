7b2b6b0df7291ed3f0c655f8368f177688bc2e56
Thomas Hancock <thomas.hancock.2012@my.bristol.ac.uk>
2021-01-28 12:15:26 +0000

    Added more db unit tests

--- internal/database/database_sqlimpl_test.go
+++ internal/database/database_sqlimpl_test.go
@@ -9,18 +9,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func clean(t *testing.T, file string) {
+	if fileExists(file) {
+		err := os.Remove(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 // Test_Database tests the basic functionality of the database
 func Test_Database(t *testing.T) {
 	testLog := &utils.TestLogger{}
 	dbFile := "sqlite-test.db"
 
 	// Remove file if it already exists
-	if fileExists(dbFile) {
-		err := os.Remove(dbFile)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+	clean(t, dbFile)
 
 	testDB := NewSQLDatabase(testLog, dbFile)
 	expectedLog := "Created SQL DB\n"
@@ -49,4 +53,77 @@ func Test_Database(t *testing.T) {
 
 	expected := "Test"
 	assert.Equal(t, entry, expected)
+
+	clean(t, dbFile)
+}
+
+// Test_Database_NoEntry tests when get is called with no matching entry
+func Test_Database_NoEntry(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	dbFile := "sqlite-test.db"
+
+	// Remove file if it already exists
+	clean(t, dbFile)
+
+	testDB := NewSQLDatabase(testLog, dbFile)
+	expectedLog := "Created SQL DB\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+
+	nEntries, err := testDB.NEntries()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, nEntries, 0)
+
+	// Retrieve entry from empty DB
+	entry, err := testDB.Get("0")
+
+	assert.NotNil(t, err)
+	expectedErr := "Unable to find entry for 0 in db: sql: no rows in result set"
+	assert.Equal(t, err.Error(), expectedErr)
+
+	expected := ""
+	assert.Equal(t, entry, expected)
+
+	expectedLog = "Unable to find entry for 0 in db\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+
+	clean(t, dbFile)
+}
+
+// Test_Database_Persistence tests the database can be loaded correctly
+func Test_Database_Persistence(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	dbFile := "sqlite-test.db"
+
+	// Remove file if it already exists
+	clean(t, dbFile)
+
+	testDB := NewSQLDatabase(testLog, dbFile)
+	expectedLog := "Created SQL DB\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+
+	// Check database is empty
+	nEntries, err := testDB.NEntries()
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, nEntries, 0)
+
+	// Add an entry
+	testDB.Add("0", "Test")
+
+	// Open database again
+	testDB_New := NewSQLDatabase(testLog, dbFile)
+
+	// Retreive persisted entry
+	entry, err := testDB_New.Get("0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "Test"
+	assert.Equal(t, entry, expected)
+
+	clean(t, dbFile)
 }
