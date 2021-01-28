3766f8889bb8a6414d41b25e56f8c6bd3ab75ed9
Thomas Hancock <thomas.hancock.2012@my.bristol.ac.uk>
2021-01-28 11:42:31 +0000

    Added unit tests for shortenhandler

---
+++ internal/handlers/shortenhandler_test.go
@@ -0,0 +1,63 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"../utils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test_ShortenHandler_EmptyDB tests ShortenHandler when the db is empty
+func Test_ShortenHandler_EmptyDB(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	testDB := utils.NewTestDatabase(testLog)
+
+	// testDB.Add("0", "TestEntry")
+
+	sh := NewShortenHandler(testLog, testDB)
+
+	req, err := http.NewRequest("GET", "/shorten?url=Test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	sh.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+
+	expected := "Test can now be accessed on /0\n"
+	assert.Equal(t, rr.Body.String(), expected)
+
+	expectedLog := "Test can now be accessed on /0\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+}
+
+// Test_ShortenHandler_EntryExists tests ShortenHandler when the db already has entries
+func Test_ShortenHandler_EntryExists(t *testing.T) {
+	testLog := &utils.TestLogger{}
+	testDB := utils.NewTestDatabase(testLog)
+
+	testDB.Add("0", "TestEntry")
+
+	sh := NewShortenHandler(testLog, testDB)
+
+	req, err := http.NewRequest("GET", "/shorten?url=Test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	sh.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+
+	expected := "Test can now be accessed on /1\n"
+	assert.Equal(t, rr.Body.String(), expected)
+
+	expectedLog := "Test can now be accessed on /1\n"
+	assert.Equal(t, testLog.PrevLog, expectedLog)
+}
